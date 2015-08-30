4fd087c25108e0ed638770e243b1fda25677946c
swang <sfwang2@gmail.com>
2015-08-30 16:08:26 -0500

    sharma commit

--- DemoProject/src/HelloWorld.java
+++ DemoProject/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Commit Made By Johir");// Don't Edit or Delete this
+		System.out.println("Commit Made By sharma");// Don't Edit or Delete this
 
 	}
 
